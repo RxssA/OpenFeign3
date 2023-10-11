3b13a20a8d3d9319e5bdfcd6ad4f6b33f247c91f
G00401655@atu.ie <G00401655@atu.ie>
2023-10-11 15:35:20 +0100

    1

---
+++ src/main/java/ie/atu/regservice/UserDetails.java
@@ -0,0 +1,12 @@
+package ie.atu.regservice;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDetails {
+    private String name;
+    private String email;
+
+}
